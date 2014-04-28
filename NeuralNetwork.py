b0cd24e380287f8639569d7b7895b8ac677f0cd5
maeberli <marco.aeberli@rae.ch>
2014-04-28 22:52:43 +0200

    Implemented convergence criteria

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -41,16 +41,23 @@ class Neuron:
         sigma_x = Neuron.sigma(x)
         return sigma_x - sigma_x * sigma_x
 
+    @staticmethod
+    def equal(actual, prediction):
+        return abs(actual - prediction) < 0.1
+
 
 class NeuralNetwork:
-    NB_HIDDEN_NEURON = 2
+    NB_MAX_ITERATIONS = 10
+    NB_HIDDEN_NEURON = 3
     ALPHA_LEARNING_RATE = 0.2
 
     def __init__(self, inputs):
         self.nb_max_inputs = len(inputs)
         self.hidden_neurons = []
+        print("Init final neuron: ")
         self.final_neuron = Neuron(range(0, NeuralNetwork.NB_HIDDEN_NEURON))
 
+        print("Init hidden neuron: ")
         self._init_hidden_neuron(inputs)
 
     def _init_hidden_neuron(self, inputs):
@@ -62,25 +69,30 @@ class NeuralNetwork:
 
         safety_counter = 0
         while not convergence:
+            no_changement = True
             for entry in learning_set:
                 actual = self.classify(entry[0])
                 prediction = entry[1]
-                delta_k = NeuralNetwork.delta_k(actual, prediction)
+                if not Neuron.equal(actual, prediction):
+                    delta_k = NeuralNetwork.delta_k(actual, prediction)
 
-                weights_jk = [self.final_neuron.input_weights[k] for k in self.final_neuron.input_weights.keys()]
+                    weights_jk = [self.final_neuron.input_weights[k] for k in self.final_neuron.input_weights.keys()]
 
-                delta_j_dict = {}
-                for neuron in self.hidden_neurons:
-                    delta_j_dict[neuron] = NeuralNetwork.delta_j(neuron.last_output, delta_k, weights_jk)
+                    delta_j_dict = {}
+                    for neuron in self.hidden_neurons:
+                        delta_j_dict[neuron] = NeuralNetwork.delta_j(neuron.last_output, delta_k, weights_jk)
 
-                self.final_neuron.apply_back_propagation(NeuralNetwork.ALPHA_LEARNING_RATE, delta_k)
+                    self.final_neuron.apply_back_propagation(NeuralNetwork.ALPHA_LEARNING_RATE, delta_k)
 
-                for neuron in self.hidden_neurons:
-                    neuron.apply_back_propagation(NeuralNetwork.ALPHA_LEARNING_RATE, delta_j_dict[neuron])
+                    for neuron in self.hidden_neurons:
+                        neuron.apply_back_propagation(NeuralNetwork.ALPHA_LEARNING_RATE, delta_j_dict[neuron])
+
+                    no_changement = False
 
             safety_counter += 1
 
-            if safety_counter > 1000:
+            print("Iteration %s" % safety_counter)
+            if no_changement or safety_counter >= NeuralNetwork.NB_MAX_ITERATIONS:
                 convergence = True
                 print("Convergence reached")
 
@@ -88,26 +100,21 @@ class NeuralNetwork:
         for neuron in self.hidden_neurons:
             print(u"Neuron final configuration: %s" % neuron.input_weights)
 
-
     @staticmethod
     def hidden_layer_error(output_i, delta_j):
         return output_i * delta_j
-
 
     @staticmethod
     def output_layer_error(output_j, delta_k):
         return output_j * delta_k
 
-
     @staticmethod
     def delta_k(output_k, target_k):
         return output_k * (1 - output_k) * (output_k - target_k)
 
-
     @staticmethod
     def delta_j(output_j, delta_k, weights_jk):
         return output_j * (1 - output_j) * sum([w * delta_k for w in weights_jk])
-
 
     def classify(self, input_vector):
         assert len(input_vector) == self.nb_max_inputs
@@ -117,6 +124,6 @@ class NeuralNetwork:
             hidden_neuron_results[i] = self.hidden_neurons[i].calc(input_vector)
 
         final = self.final_neuron.calc(hidden_neuron_results)
-
-        return final
+        print("final: %s" % final)
+        return 1.0 if Neuron.equal(final, 1.0) else 0.0
 
